b4eb0c3621dceb03254c9100fe3fbbf92e05c723
ivi0876 <sanivi08@gmail.com>
2024-09-18 12:45:26 +0200

    changes

--- Hola2.java
+++ Hola2.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 public class Hola2 {
+    //static Scanner sc = new Scanner(System.in);
     public static void main(String args[]) {
         // Declaración de variables
-        String nombre;
+        String nombre; //Variable que guarda una cadena de caracteres
         int edad;
+        // Comprobar si meter como variable global, porque si no hay que cerrar el scanner, con sc.closed()
         Scanner sc = new Scanner(System.in);
         // Código
         System.out.print("Dime tu nombre: ");
         nombre = sc.nextLine();
         System.out.println("Ahora tu edad");
         edad = sc.nextInt();
+        //sc.close();
         System.out.println(nombre + ", Welcome to the Java World");
         System.out.println("Nos vemos, ser humano de " + edad + " años");
     }
