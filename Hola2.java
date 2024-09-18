cd3a8b6ad659b26d786b7f01209059aac651917e
ivi0876 <sanivi08@gmail.com>
2024-09-18 13:54:02 +0200

    comment

--- Hola2.java
+++ Hola2.java
@@ -9,9 +9,9 @@ public class Hola2 {
         Scanner sc = new Scanner(System.in);
         // Código
         System.out.print("Dime tu nombre: ");
-        nombre = sc.nextLine();
+        nombre = sc.nextLine(); // lee cadena de caracteres
         System.out.println("Ahora tu edad");
-        edad = sc.nextInt();
+        edad = sc.nextInt(); // lee numeros
         //sc.close();
         System.out.println(nombre + ", Welcome to the Java World");
         System.out.println("Nos vemos, ser humano de " + edad + " años");
