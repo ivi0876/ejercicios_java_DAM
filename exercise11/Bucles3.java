9ce67c4cc2119075e0121954ad55efed6a13aa64
ivi0876 <sanivi08@gmail.com>
2024-09-19 22:23:08 +0200

    exercise11

---
+++ exercise11/Bucles3.java
@@ -0,0 +1,33 @@
+package exercise11;
+
+public class Bucles3 {
+    public static void main(String[] args) {
+        // ?PREGUNTAR SI PUEDO PONER VARIABLE GENERAL PARA TODO
+        int acu = 0;
+        int cont = 1;
+        //Primer bucle uso for porque necesitamos mostrar la evolucion
+        System.out.println("Usar el bucle for: ");
+        for (cont = 1; cont <= 20; cont ++) {
+            acu += acu + cont; // !PREGUNTAR SI ESTO ES EQUIVALENTE A: acu=acu+c
+            System.out.println("Contador: " + cont + ", acumulador: " + acu);
+        }
+
+        // Segundo bucle usando while porque solo hay que mostrar el final
+        System.out.println("Usando bucle while: ");
+        while (cont <=20) {
+            acu += cont;
+            cont ++;
+        }
+        System.out.println("El final: " + acu);
+
+        // tercer bucle usando do while
+        System.out.println("Usando bucle do while: ");
+        do{
+            acu += cont;
+            cont ++;
+        } while (cont <= 20);
+        System.out.println("El final: " + acu);
+    }
+}
+
+//! PREGUNTAR PORQUE LOS RESULTADOS NO SON IGUALES
