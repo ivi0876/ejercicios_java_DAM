48fc685ff35828e25db2bcbeee1c1e2494d95698
ivi0876 <sanivi08@gmail.com>
2024-09-19 18:58:50 +0200

    changes

--- Exercise1/Basico.java
+++ Exercise1/Basico.java
@@ -5,21 +5,17 @@ public class Basico {
     public static void main(String args[]) {
         System.out.println("Bienvenidos a mi superprograma v1.0");
         System.out.println("Ahora unas preguntas");
-        System.out.println("Dime tu nombre");
+        System.out.print("Dime tu nombre: ");
         String nombre;
         Scanner sc = new Scanner(System.in);
         nombre = sc.nextLine();
-        System.out.println("Dime tu dirección");
+        System.out.print("Dime tu dirección: ");
         String direccion;
         direccion = sc.nextLine();
         int edad;
-        System.out.println("Dime tu edad");
+        System.out.print("Dime tu edad: ");
         edad = sc.nextInt();
-        //System.out.println("Nombre: " + nombre);
-        //System.out.println("Direccion: " + direccion);
-        //System.out.println("Edad: " + edad);
-        System.out.print("Te llamas " + nombre);
-        System.out.println(" y vives en " + direccion);
-        System.out.println(" y tienes " + edad);
+        System.out.println("Te llamas " + nombre + " y vives en " + direccion + " y tienes " + edad + ".");
+             
     }
 }
