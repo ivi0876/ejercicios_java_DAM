275189fd85304e24a0e63b907a9f848cdba4203d
ivi0876 <sanivi08@gmail.com>
2024-09-20 09:36:22 +0200

    exercise4

--- Boletin2/exercise4/Bol2_Ejer4.java
+++ Boletin2/exercise4/Bol2_Ejer4.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 @SuppressWarnings({ "resource" })
 public class Bol2_Ejer4 {
     public static void main(String[] args) {
-        System.out.println("Dime un numero entero: ");
+        System.out.print("Dime un numero entero: ");
         Scanner sc = new Scanner(System.in);
         int numero1 = sc.nextInt();
-        System.out.println("Dime un numero real: ");
+        System.out.print("Dime un numero real: ");
         //double numero2 = sc.nextDouble();
         System.out.printf("El numero entero en decimal es: %d", numero1);
         System.out.println();
