7c1d7fa491fe28615250a0546a4970c9e6528c80
ivi0876 <sanivi08@gmail.com>
2024-09-21 10:48:30 +0200

    exercise6

--- Boletin2/exercise6/Bol2_Ejer6.java
+++ Boletin2/exercise6/Bol2_Ejer6.java
@@ -1,7 +1,12 @@
 package Boletin2.exercise6;
+import java.util.Scanner;
+@SuppressWarnings({ "resource" })
 
 public class Bol2_Ejer6 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\u001b[2J\u001b[H");
+        
         
     }
 }
