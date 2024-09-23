e5ada2681305185dfc59b49970936676072d42b2
ivi0876 <sanivi08@gmail.com>
2024-09-23 11:09:57 +0200

    exercise8

--- Boletin2/exercise8/Bol2_Ejer8.java
+++ Boletin2/exercise8/Bol2_Ejer8.java
@@ -3,7 +3,22 @@ import java.util.Scanner;
 @SuppressWarnings({ "resource" })
 public class Bol2_Ejer8 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      // Solicitar los tres números al usuario
+        System.out.print("Introduce el primer número: ");
+        int num1 = sc.nextInt();
         
+        System.out.print("Introduce el segundo número: ");
+        int num2 = sc.nextInt();
+        
+        System.out.print("Introduce el tercer número: ");
+        int num3 = sc.nextInt();
+        // usar ternaria para optimizar comparaciones
+        int mayor = (num1 >= num2) ? ((num1 >= num3) ? num1 : num3) : ((num2 >= num3) ? num2 : num3);
 
-    }
+        // Mostrar el resultado
+       System.out.println("El mayor de los tres números es: " + mayor);
+
+    }   
 }
+
