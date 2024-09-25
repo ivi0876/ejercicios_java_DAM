018f5369fae19223b801f89e6d2111a54148aeae
ivi0876 <sanivi08@gmail.com>
2024-09-25 13:02:56 +0200

    exercise14

--- Boletin2/exercise14/Bol2_Ejer14.java
+++ Boletin2/exercise14/Bol2_Ejer14.java
@@ -5,3 +5,18 @@ public class Bol2_Ejer14 {
         
     }
 }
+
+/*
+double cantidad = 0;
+                    int contador = 0;
+                    double numero;
+                    do {
+                        System.out.print("Introduce un número (0 para terminar): ");
+                        numero = sc.nextDouble();
+                        if (numero != 0) {
+                            sumaIndefinida += numero;
+                            contador++;
+                        }
+                    } while (numero != 0);
+
+*/
