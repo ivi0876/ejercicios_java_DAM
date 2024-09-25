2c5ac686a1a959956cdac387a8b000b2f4ef1721
ivi0876 <sanivi08@gmail.com>
2024-09-25 06:44:07 +0200

    exercise11

--- Boletin2/exercise11/Bol2_Ejer11.java
+++ Boletin2/exercise11/Bol2_Ejer11.java
@@ -2,6 +2,16 @@ package Boletin2.exercise11;
 
 public class Bol2_Ejer11 {
     public static void main(String[] args) {
-        
+        int suma = 0; // Inicializamos la suma
+        int N = 1;    // Inicializamos N
+
+        // Bucle que se ejecuta hasta que la suma sea mayor que 10000
+        while (suma <= 10000) {
+            suma += N; // Sumamos N a la suma acumulada
+            N++;       // Incrementamos N
+        }
+
+        // ! IMPORTANTE  valor de N - 1 porque al salir del bucle N ya se ha incrementado
+        System.out.println("El primer valor de N para el cual la suma supera 10000 es: " + (N-1));
     }
 }
