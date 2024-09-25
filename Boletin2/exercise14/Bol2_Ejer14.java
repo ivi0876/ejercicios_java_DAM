63e82205ff747c4e5c0b76c0fd51f9c7a345b307
ivi0876 <sanivi08@gmail.com>
2024-09-25 13:19:48 +0200

    exercise14

--- Boletin2/exercise14/Bol2_Ejer14.java
+++ Boletin2/exercise14/Bol2_Ejer14.java
@@ -1,22 +1,31 @@
 package Boletin2.exercise14;
 
+import java.util.Scanner;
+@SuppressWarnings({ "resource" })
+
 public class Bol2_Ejer14 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int TotalNumeros = 0;
+        int negativos = 0; 
+        int numero;
+
+        do {
+            System.out.print("Introduce número: ");
+            numero = sc.nextInt();
+            
+            if (numero != 0) { // No contamos el 0
+                TotalNumeros++; // Incrementa el total de números introducidos
+                
+                if (numero < 0) {
+                    negativos++; // Incrementa la cantidad de números negativos
+                }
+            }
+        } while (numero != 0); // Sale del bucle cuando el usuario introduce 0
+        
+        // Muestra el resultado
+        System.out.println("Se introdujeron " + negativos + " números negativos de un total de " + TotalNumeros);
         
     }
 }
-
-/*
-double cantidad = 0;
-                    int contador = 0;
-                    double numero;
-                    do {
-                        System.out.print("Introduce un número (0 para terminar): ");
-                        numero = sc.nextDouble();
-                        if (numero != 0) {
-                            sumaIndefinida += numero;
-                            contador++;
-                        }
-                    } while (numero != 0);
-
-*/
