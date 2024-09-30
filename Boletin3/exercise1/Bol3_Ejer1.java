a56ebe90ef97092652a547ebc55829565c9a0252
ivi0876 <sanivi08@gmail.com>
2024-09-30 11:54:42 +0200

    exercise1

--- Boletin3/exercise1/Bol3_Ejer1.java
+++ Boletin3/exercise1/Bol3_Ejer1.java
@@ -9,11 +9,21 @@ public class Bol3_Ejer1 {
             System.out.println("\n");
         }        
     }
+
+    public static void par(int num){
+        if(num%2 == 0) {
+            System.out.printf("El numero es par");
+        }else {
+            System.out.println("El numero no es par");
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
+        int num=10;
         System.out.print("Cuantas lineas en blanco quieres: ");
         n = sc.nextInt();
         lineasblanco(n);
+        par(num);
     }
 }
