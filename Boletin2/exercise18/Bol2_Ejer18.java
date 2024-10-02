cf6487005c53977efb0cc350678654dc5e7afee4
ivi0876 <sanivi08@gmail.com>
2024-10-02 12:28:57 +0200

    exercise18

--- Boletin2/exercise18/Bol2_Ejer18.java
+++ Boletin2/exercise18/Bol2_Ejer18.java
@@ -1,22 +1,50 @@
+
 package Boletin2.exercise18;
 import java.util.Scanner;
 @SuppressWarnings({ "resource" })
-
+// *= multiplico y igualo, es una forma de resumir la operacion
 
 public class Bol2_Ejer18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         long numero;
-        double factorial = 1;
-		
-        do {  //introducir por teclado un número mayor o igual a cero
-            System.out.print("Introduce un numero entero: ");
-            numero = sc.nextInt();
-        } while (numero < 0);
-        for (long i = 1; i <= numero; i++) {
-            factorial = factorial * i;
-        }
+        long factorial;
 
-        System.out.println("El número en factorial: " + factorial);
+        // Solicitar un número entero no negativo
+        do {
+            System.out.print("Introduce un número entero (mayor o igual a 0): ");
+            numero = sc.nextLong();
+        } while (numero < 0);
+
+        // bucle for
+        factorial = 1;
+        for (long i = 1; i <= numero; i++) {
+            factorial *= i;
+        }
+        System.out.println("El factorial de " + numero + " usando bucle for es: " + factorial);
+        
+        // bucle while
+        factorial = 1; 
+        long i = 1;
+        while (i <= numero) {
+            factorial *= i;
+            i++;
+        }
+        System.out.println("El factorial de " + numero + " usando bucle while es: " + factorial);
+        
+        // bucle do-while
+        factorial = 1; 
+        i = 1;
+        do {
+            factorial *= i;
+            i++;
+        } while (i <= numero);
+
+        // Asegurarse de que factorial sea 1 cuando el número es 0
+        factorial *= (numero == 0) ? 1 : 1; // *= multiplico y igualo, es una forma de resumir la operacion
+        System.out.println("El factorial de " + numero + " usando bucle do-while es: " + factorial);
+        
     }
 }
+
