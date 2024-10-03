09af8d5d88290db129540059b898cb38efa71751
ivi0876 <sanivi08@gmail.com>
2024-10-03 13:56:43 +0200

    exercise3

--- Boletin3/exercise3/Bol3_Ejer3.java
+++ Boletin3/exercise3/Bol3_Ejer3.java
@@ -1,7 +1,18 @@
 package Boletin3.exercise3;
 
 public class Bol3_Ejer3 {
+
+
+    /**
+     * @param year Año intrucido.
+     * @return true si bisiesto, false si no lo es.
+     */
+
+     public static boolean bisiesto(int year) {
+     return year %2 == 0;
+    }
+
     public static void main(String[] args) {
-        
+        bisiesto(1980);
     }
 }
