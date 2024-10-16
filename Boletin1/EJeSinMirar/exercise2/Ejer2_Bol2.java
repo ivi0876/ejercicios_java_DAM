a07986c0a149f1f435b179a2f3f729b4a0773a03
ivi0876 <sanivi08@gmail.com>
2024-10-16 18:24:21 +0200

    exercise2

--- Boletin1/EJeSinMirar/exercise2/Ejer2_Bol2.java
+++ Boletin1/EJeSinMirar/exercise2/Ejer2_Bol2.java
@@ -1,7 +1,11 @@
 package Boletin1.EJeSinMirar.exercise2;
-
+import java.util.Scanner;
 public class Ejer2_Bol2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int numero = sc.nextInt();
+        System.out.println(numero);
+        sc.close();
     }
+    
 }
