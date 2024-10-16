1ade9967452df272c2a49f4d6d0a227b75ae3ae7
ivi0876 <sanivi08@gmail.com>
2024-10-16 16:41:24 +0200

    boletinguiado3

---
+++ boletinguiado3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class boletinguiado3 {
+    public static void main(String[] args) {
+        // Llamamos a la función pideNumero con el mensaje y guardamos el valor retornado
+        int numero = pideNumero("Dime un número y te diré el doble");
+        // Mostramos el doble del número devuelto
+        System.out.println("El doble del número es: " + (numero * 2));
+    }
+
+    // Definimos la función pideNumero
+    public static int pideNumero(String msg) {
+        // Creamos un objeto Scanner para leer la entrada del usuario
+        Scanner sc = new Scanner(System.in);
+        // Mostramos el mensaje proporcionado como parámetro
+        System.out.println(msg);
+        // Leemos un número entero ingresado por el usuario
+        int numero = sc.nextInt();
+        // Devolvemos el número leído
+        return numero;
+    }
+}
