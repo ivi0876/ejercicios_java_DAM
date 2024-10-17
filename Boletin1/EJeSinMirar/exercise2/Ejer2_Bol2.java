04117623645cb78a0aa577cfc03072252e924983
ivi0876 <sanivi08@gmail.com>
2024-10-17 12:35:43 +0200

    exercise2

--- Boletin1/EJeSinMirar/exercise2/Ejer2_Bol2.java
+++ Boletin1/EJeSinMirar/exercise2/Ejer2_Bol2.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class Ejer2_Bol2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numero = sc.nextInt();
-        System.out.println(numero);
+        System.out.print("Dime el primer numero: ");
+        int numero1 = sc.nextInt();
+        System.out.print("Dime el segundo numero: ");
+        int numero2 = sc.nextInt();
+
+        int suma = numero1 + numero2;
+        System.out.println("EL resultado de la suma entre "+ numero1 + " y " + numero2 +" es: " + suma);
         sc.close();
     }
     
