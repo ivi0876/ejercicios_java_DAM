05e322d2cb7bde46f1fbc92ad24d7bad802ef08f
ivi0876 <sanivi08@gmail.com>
2024-10-18 09:02:50 +0200

    exercise5

--- Boletin1/EJeSinMirar/exercise5/Ejer5_Bol1.java
+++ Boletin1/EJeSinMirar/exercise5/Ejer5_Bol1.java
@@ -1,7 +1,18 @@
 package Boletin1.EJeSinMirar.exercise5;
-
+import java.util.Scanner;
 public class Ejer5_Bol1 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+        int edad;
+        double temperatura;
+        System.out.print("Introduce tu nombre: ");
+        nombre = sc.nextLine();
+        System.out.print("Introduce tu edad: ");
+        edad = sc.nextInt();
+        System.out.print("Introduce tu temperatura: ");
+        temperatura = sc.nextDouble();
+        sc.close();
+        System.out.println("Tu nombre es: " + nombre + "\n tienes " + edad + " años " + "\nhace " + temperatura + " grados");
     }
 }
--- Boletin1/EJeSinMirar/exercise5/readme.md
+++ Boletin1/EJeSinMirar/exercise5/readme.md
@@ -1,4 +1,3 @@
 Escribe un programa que pida al usuario su nombre, la edad (entero) y la
-temperatura actual (real). Debe mostrar luego por pantalla los tres datos, cada
-uno en una linea, pero usando un único comando System.out.println (debes
+temperatura actual (real). Debe mostrar luego por pantalla los tres datos, cada uno en una linea, pero usando un único comando System.out.println (debes
 concatenar los retornos de carro).
