a4c6d5401f5c96fb20ef351fb37f4438dd38ee0f
ivi0876 <sanivi08@gmail.com>
2024-12-05 09:04:42 +0100

    update

--- POO/Bol4_1/Ejer2/Fecha.java
+++ POO/Bol4_1/Ejer2/Fecha.java
@@ -5,14 +5,12 @@ public class Fecha {
     private int mes;
     private int year;
 
-
-
     // Constructor vacío que inicializa a una fecha por defecto
-        public Fecha() {
-            this.dia = 3;
-            this.mes = 12;
-            this.year = 2024;
-        }
+    public Fecha() {
+        this.dia = 3;
+        this.mes = 12;
+        this.year = 2024;
+    }
 
     // Constructor con parámetros
     public Fecha(int dia, int mes, int year) {
@@ -43,5 +41,25 @@ public class Fecha {
         } else {
             this.mes = 1;
         }
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String fechaFormateada(boolean formato) {
+        if (formato) {
+            return dia + "/" + mes + "/" + year;
+        } else {
+            
+        }
+    }
+
+    public static int diferenciaFechas(Fecha fecha1, Fecha fecha2) {
+        return Math.abs(fecha1.getYear() - fecha2.getYear());
     }
 }
--- POO/Bol4_1/Ejer2/Principal.java
+++ POO/Bol4_1/Ejer2/Principal.java
@@ -1,7 +1,39 @@
 package POO.Bol4_1.Ejer2;
 
+import java.util.Scanner;
+
+
 public class Principal {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        // primera fecha
+        System.out.println("Introduce la primera fecha:");
+        System.out.print("Día: ");
+        int dia1 = sc.nextInt();
+        System.out.print("Mes: ");
+        int mes1 = sc.nextInt();
+        System.out.print("Año: ");
+        int año1 = sc.nextInt();
+
+        // segunda fecha 
+        System.out.println("Introduce la segunda fecha:");
+        System.out.print("Día: ");
+        int dia2 = sc.nextInt();
+        System.out.print("Mes: ");
+        int mes2 = sc.nextInt();
+        System.out.print("Año: ");
+        int año2 = sc.nextInt();
+
+
+        Fecha fecha1 = new Fecha(dia1, mes1, año1);
+        Fecha fecha2 = new Fecha(dia2, mes2, año2);
+
+        System.out.println("\nFecha 1 en formato corto: " + fecha1.fechaFormateada(true));
+        System.out.println("Fecha 1 en formato largo: " + fecha1.fechaFormateada(false));
+        System.out.println("\nFecha 2 en formato corto: " + fecha2.fechaFormateada(true));
+        System.out.println("Fecha 2 en formato largo: " + fecha2.fechaFormateada(false));
+
+        int diferenciaYear = Fecha.diferenciaFechas(fecha1, fecha2);
+        System.out.println("\nDiferencia en años entre las dos fechas: " + diferenciaYear);
     }
 }
