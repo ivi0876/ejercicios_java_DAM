9ab520265f040eb422966af409236affb7f9c5e1
ivi0876 <sanivi08@gmail.com>
2024-12-11 16:51:48 +0100

    update

--- POO/ProyectoEmpresa/Empresa.java
+++ POO/ProyectoEmpresa/Empresa.java
@@ -1,5 +1,18 @@
 package POO.ProyectoEmpresa;
 
 public class Empresa {
-    
+    private double ganancias;
+    Directivo directivo = new Directivo();
+    Empleado empleado1 = new Empleado();
+    Empleado empleado2 = new Empleado();
+
+
+    public void setGanacias(double ganancias){
+        this.ganancias = ganancias;
+
+    }
+
+    public double getGanancias(){
+        return ganancias;
+    }
 }
