73f4b6cb703baa53b689d4bf10284b9c55086cd1
ivi0876 <sanivi08@gmail.com>
2025-01-08 13:48:23 +0100

    update

--- Arrays/Unidimensionales.java
+++ Arrays/Unidimensionales.java
@@ -4,7 +4,29 @@ public class Unidimensionales {
 
     public static void main(String[] args) {
         double[]  temperaturas = new double[4];
+       // char[] letras = {'a', 'b', 'c', 'd'}; // unica excepcion que puedo meter los caracteres en la misma linea sin tener que poner NEW
 
-       
+        // temperaturas[0] = 7.5;
+        // temperaturas[1] = 3.2;
+        // temperaturas[2] = 1.3;
+        // temperaturas[3] = 10;
+
+        // Rellena un array con valores aleatorios
+        for (int i = 0; i < temperaturas.length; i++){
+            temperaturas[i]=Math.random()*50-25;
+        }
+
+        System.out.println("Tamaño: " + temperaturas.length);
+
+
+
+
+       // for(int i = 0; i<= temperaturas.length-1 ;i++)  misma forma pero se usa mas la segunda
+        for(int i = 0; i< temperaturas.length ;i++) { // lenght me da el tamaño del array
+
+            System.out.printf("Indice:%2d valor:%7.2fºC\n", i, temperaturas[i]);
+          //  System.out.printf("Indice:[%d] = %7.2fºC\n", i, temperaturas[i]); misma forma pero diferente formato
+        }
+        
     }
-}
+}   
