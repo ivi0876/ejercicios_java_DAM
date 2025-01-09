e2f02541a8aa3d7935ff6f414ab262edc0894282
ivi0876 <sanivi08@gmail.com>
2025-01-09 09:37:01 +0100

    update

--- Arrays/Actividad1/Actividad1.java
+++ Arrays/Actividad1/Actividad1.java
@@ -1,8 +1,9 @@
 package Arrays.Actividad1;
 
 import java.util.Scanner;
-
+@SuppressWarnings("resource")
 public class Actividad1 {
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[10];
