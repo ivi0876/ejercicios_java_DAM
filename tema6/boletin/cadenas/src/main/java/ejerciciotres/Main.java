f0765a5dd975ac3dfab8adca3ecb5cc5f6252536
ivi0876 <50300489+ivi0876@users.noreply.github.com>
2025-02-07 11:10:26 +0100

    update

---
+++ tema6/boletin/cadenas/src/main/java/ejerciciotres/Main.java
@@ -0,0 +1,26 @@
+package ejerciciotres;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear un objeto Cadena y asignarle un valor
+        Cadena c = new Cadena();
+        c.setCadena("hola mundo");
+        System.out.println("Cadena: " + c.toString());
+
+        // Comparación con un String
+        System.out.println("¿Es igual a 'hola mundo'? " + c.equals("hola mundo"));
+
+        // Comparacion con otro objeto Cadena
+        Cadena otraCadena = new Cadena();
+        otraCadena.setCadena("hola mundo");
+        System.out.println("¿Es igual a otro Cadena con 'hola mundo'? " + c.equals(otraCadena));
+
+        // Comparación con un array de caracteres
+        char[] array = {'h', 'o', 'l', 'a', ' ', 'm', 'u', 'n', 'd', 'o'};
+        System.out.println("¿Es igual a un array de char? " + c.equals(array));
+
+        // Prueba del método eliminar(char)
+        System.out.println("Eliminando 'o'... Eliminados: " + c.eliminar('o'));
+        System.out.println("Cadena después de eliminar: " + c.toString());
+    }
+}
